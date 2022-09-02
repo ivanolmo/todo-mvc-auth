f85b75322b7527864700a1d61d0044cd5828322e
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-09-02 17:07:57 -0500

    Updated tags controller to ignore blank tags

--- controllers/tags.js
+++ controllers/tags.js
@@ -19,23 +19,23 @@ module.exports = {
   },
   createTags: async (req, res) => {
     // Create create tags function
-    let tags = req.body.tags.split(',').map(e => ({
-      updateOne: {
-        filter: {tag: e.toLowerCase().trim()},
-        update: {
-          tag: e.toLowerCase().trim(),
-          userId: req.user.id
-        },
-        upsert: true
-      }
-    }))
     try {
-      const tagResult = await Tag.bulkWrite(tags); // Create tags
+      const tagIds = [] // Stores ids of all tags, new or upserted
+      let tags = req.body.tags.split(',').map(e => e.toLowerCase().trim()).filter(Boolean) // Split tags by commas and filter out any empty tags
+      for (let tag of tags) { // Loop through tags to add them
+        const tagId = await Tag.findOneAndUpdate( // Using findOneAndUpdate so it will only insert tags that don't already exist, but will also return IDs of existing tags
+          { tag: tag },
+          { $set: { tag: tag, userId: req.user.id } },
+          { new: true,
+            upsert: true }
+        )
+        tagIds.push(tagId._id)
+      }
       console.log('Tags have been added!'); // Log to console
       if ( !req.body.todoItem ) {
-        res.redirect('/tags'); // Redirect to tags
+        res.redirect('/tags'); // Redirect to tags if editing tags in the Tags view
       } else {
-        return tagResult.result.upserted
+        return tagIds // Otherwise return a list of tag IDs
       }
     } catch (err) {
       // Catch
@@ -44,7 +44,6 @@ module.exports = {
   },
   deleteTag: async (req, res) => {
     // Create delete tag function
-    console.log(req.body.tagId); // Log to console
     try {
       // Try
       await Tag.findOneAndDelete({ _id: req.body.tagId }); // Find tag and delete
@@ -55,4 +54,18 @@ module.exports = {
       console.log(err); // Log to console
     }
   },
+  updateTag: async (req, res) => {
+    // Create the update tag function
+    try {
+      await Tag.findOneAndUpdate(
+        { _id: req.body.tagId },
+        { tag: req.body.tag }
+      )
+      console.log('Updated tag')
+      res.json('Updated tag')
+    } catch (err) {
+      // Catch
+      console.log(err); // Log to console
+    }
+  }
 };
